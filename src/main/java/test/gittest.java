57984ced8b8cb0eb63f07652aa5a7f9a6ecfe338
jixl <jixl@126.com>
2024-11-07 17:12:46 +0800

    hot fix first commit by jixl 20241107

--- src/main/java/test/gittest.java
+++ src/main/java/test/gittest.java
@@ -5,6 +5,6 @@ public class gittest {
         System.out.println("Hello git");
         System.out.println("Hello git222");
         System.out.println("Hello git3");
-
+        System.out.println("Hello git hot fix");
     }
 }
